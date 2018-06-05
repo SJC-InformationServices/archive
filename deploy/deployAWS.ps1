196d83dd8f6e461050fcdf5e4c683e1f541524df
Kevin <kevin.noseworthy@stjoseph.com>
2018-06-05 14:56:06 -0400

    sjcArchiveDeploy sjcArchiveDeploy-2018-06-05-145605.zip

--- deploy/deployAWS.ps1
+++ deploy/deployAWS.ps1
@@ -8,7 +8,7 @@ git add --all
 git commit -m "sjcArchiveDeploy $name"
 #git archive -v -o $name --format=zip HEAD
 #Write-S3Object -BucketName sjcarchivefiles-dev -File $name
-#aws elasticbeanstalk create-application-version --application-name "SJC_Archive" --version-label $name --description sjc_archive_dev --source-bundle S3Bucket="sjcarchivefiles-dev",S3Key=$name
+aws elasticbeanstalk create-application-version --application-name "SJC_Archive" --version-label $name --description sjc_archive_dev --source-bundle S3Bucket="sjcarchivefiles-dev",S3Key=$name
 #Update-EBEnvironment -ApplicationName "SJC_Archive" -EnvironmentName "sjcarchiveold" -VersionLabel $name
 #Remove-Item -path $name
 #aws s3 cp "C:\Users\kevin_000\Documents\DevWorkSpaces\sdmArchive\lib\sjccontent\" s3://sjcarchiveassets/lib/sjccontent/ --recursive
