acca9076557b36eab21a5f1ec233cec7b9d690b0
Kevin <kevin.noseworthy@stjoseph.com>
2018-06-05 14:12:40 -0400

    sjcArchiveDeploy sjcArchiveDeploy-2018-06-05-141239.zip

--- deploy/deployAWS.ps1
+++ deploy/deployAWS.ps1
@@ -6,16 +6,16 @@ $datestr = $date.ToString()
 $name = "sjcArchiveDeploy-"+$datestr+".zip"
 git add --all 
 git commit -m "sjcArchiveDeploy $name"
-#git archive -v -o $name --format=zip HEAD
-#Write-S3Object -BucketName sjcarchivefiles-dev -File $name
-#aws elasticbeanstalk create-application-version --application-name "SJC_Archive" --version-label $name --description sjc_archive_dev --source-bundle S3Bucket="sjcarchivefiles-dev",S3Key=$name
-#Update-EBEnvironment -ApplicationName "SJC_Archive" -EnvironmentName "sjcarchiveold" -VersionLabel $name
-#Remove-Item -path $name
+git archive -v -o $name --format=zip HEAD
+Write-S3Object -BucketName sjcarchivefiles-dev -File $name
+aws elasticbeanstalk create-application-version --application-name "SJC_Archive" --version-label $name --description sjc_archive_dev --source-bundle S3Bucket="sjcarchivefiles-dev",S3Key=$name
+Update-EBEnvironment -ApplicationName "SJC_Archive" -EnvironmentName "sjcarchiveold" -VersionLabel $name
+Remove-Item -path $name
 #aws s3 cp "C:\Users\kevin_000\Documents\DevWorkSpaces\sdmArchive\lib\sjccontent\" s3://sjcarchiveassets/lib/sjccontent/ --recursive
 
 #aws s3 cp "C:\Users\kevin_000\Documents\DevWorkSpaces\sdmArchive\lib\css\" s3://sjcarchiveassets/lib/css/ --recursive
 #aws s3 cp "C:\Users\kevin_000\Documents\DevWorkSpaces\sdmArchive\lib\apps\" s3://sjcarchiveassets/lib/apps/ --recursive
 
 #aws s3 cp "C:\Users\kevin_000\Documents\DevWorkSpaces\sdmArchive\lib\images\" s3://sjcarchiveassets/lib/images/ --recursive
-eb deploy
+#eb deploy
 #New-EBApplicationVersion -ApplicationName SJC_Archive -VersionLabel $name -SourceBuildInformation_SourceType Zip -SourceBuildInformation_SourceRepository S3 -SourceBuildInformation_SourceLocation sjcarchivefiles-dev/$name
